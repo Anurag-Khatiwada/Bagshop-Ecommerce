29e540fad09550d623821e4e676559154ccf628e
Anurag-Khatiwada <anuragkhatiwada108@gmail.com>
2024-10-28 11:16:48 +0545

    mongo

--- config/mongoose-connection.js
+++ config/mongoose-connection.js
@@ -4,7 +4,7 @@ const config = require('config');
 const dbgr = require("debug") ("development:mongoose");
 
 mongoose
-.connect(`${config.get("MONGODB_URI")}/bagshop`)
+.connect(process.env.MONGO_URI)
 .then(function(){
     dbgr("connected");
 })
