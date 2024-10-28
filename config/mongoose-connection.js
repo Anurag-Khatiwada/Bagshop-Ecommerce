57999f8d4fa9dab00e0a48211d707fdf4bb77017
Anurag-Khatiwada <anuragkhatiwada108@gmail.com>
2024-10-28 11:22:21 +0545

    yes

--- config/mongoose-connection.js
+++ config/mongoose-connection.js
@@ -1,13 +1,6 @@
 const mongoose = require("mongoose");
-const config = require('config');
-
-
 
 mongoose
 .connect(process.env.MONGO_URI)
-
-.catch(function(err){
-    dbgr(err);
-})
 
 module.exports = mongoose.connection;
