fa4fa4761155ecbf79d43b3bfb1f18c479f56998
Anurag-Khatiwada <anuragkhatiwada108@gmail.com>
2024-10-28 11:20:04 +0545

    added

--- config/mongoose-connection.js
+++ config/mongoose-connection.js
@@ -1,13 +1,11 @@
 const mongoose = require("mongoose");
 const config = require('config');
 
-const dbgr = require("debug") ("development:mongoose");
+
 
 mongoose
 .connect(process.env.MONGO_URI)
-.then(function(){
-    dbgr("connected");
-})
+
 .catch(function(err){
     dbgr(err);
 })
